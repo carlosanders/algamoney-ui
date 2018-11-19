a6334f6d0673cba58202eddbec295822f3d9622c
Carlos Anders <carlosanders@gmail.com>
2018-11-18 23:18:43 -0200

    aula 10.4 finalizada.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {TabViewModule} from 'primeng/primeng';
+
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ButtonModule } from 'primeng/components/button/button';
+
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    TabViewModule
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
