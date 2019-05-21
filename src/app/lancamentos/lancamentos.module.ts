3df78900fd179786cdf2fbe0431c6cdc3ad678da
Carlos Anders <carlosanders@gmail.com>
2019-05-21 16:08:43 -0300

    aula 18.8 ok

--- src/app/lancamentos/lancamentos.module.ts
+++ src/app/lancamentos/lancamentos.module.ts
@@ -1,28 +1,28 @@
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
-import { ButtonModule } from 'primeng/components/button/button';
-import { DataTableModule } from 'primeng/components/datatable/datatable';
-import { TooltipModule } from 'primeng/components/tooltip/tooltip';
-import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
-import { CalendarModule } from 'primeng/components/calendar/calendar';
-import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
-import { DropdownModule } from 'primeng/components/dropdown/dropdown';
-import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 
-// meus imports
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { SharedModule } from './../shared/shared.module';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import {SharedModule} from '../shared/shared.module';
-
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule,
+
     InputTextModule,
     ButtonModule,
     DataTableModule,
@@ -31,7 +31,6 @@ import {SharedModule} from '../shared/shared.module';
     CalendarModule,
     SelectButtonModule,
     DropdownModule,
-    InputMaskModule,
     CurrencyMaskModule,
 
     SharedModule
